7c87c3f8e0b7ac14040b794493a11a83a00951d7
psiorx <psiorx@gmail.com>
2017-07-24 20:36:06 -0400

    game class done

--- src/test.cpp
+++ src/test.cpp
@@ -6,70 +6,121 @@
 //each agent takes a turn at modifying the board state
 //until a terminal state is reached
 
-enum class BoardStatus {
+enum class GameStatus {
   X_WINS,
   O_WINS,
-  IN_PROGRESS
+  X_TURN,
+  O_TURN
 };
 
-std::string to_string(BoardStatus status) {
+std::string to_string(GameStatus status) {
   switch(status) {
-    case BoardStatus::X_WINS:
+    case GameStatus::X_WINS:
       return "X_WINS";
-    case BoardStatus::O_WINS:
+    case GameStatus::O_WINS:
       return "O_WINS";
-    case BoardStatus::IN_PROGRESS:
-      return "IN_PROGRESS";
+    case GameStatus::X_TURN:
+      return "X_TURN";
+    case GameStatus::O_TURN:
+      return "O_TURN";
   }
   return "UNKNOWN";
 }
+
+struct TicTacToeAction {
+  int column_index;
+  int row_index;
+  char value;
+};
 
 
 template <int size>
 class TicTacToeBoard {
   public:
   using BoardState = Eigen::Matrix<char, size, size>;
-  TicTacToeBoard() { 
-    board_state_.fill('_');
-    board_state_(2, 0) = 'x';
-    board_state_(1, 1) = 'x';
-    board_state_(0, 2) = 'x';
+  TicTacToeBoard() {
+    Reset(); 
   };
 
-  BoardState GetBoardState() {
+  BoardState GetBoardState() const {
     return board_state_;
   }
+  
+  void Reset() {
+    game_status_ = GameStatus::X_TURN;
+    board_state_.fill('_');
+    x_turn_ = true;
+  }
 
-  BoardStatus CheckBoardStatus() {
-   //check rows
-  for(int row_index = 0; row_index < size; row_index++) {
-    if(CheckRowValue(row_index, 'o')) {
-      return BoardStatus::O_WINS;
-    } else if(CheckRowValue(row_index, 'x')) {
-      return BoardStatus::X_WINS;
+  bool IsActionValid(TicTacToeAction const& action) const {
+    if(GameOver()) return false;
+    bool correct_turn = game_status_ == GameStatus::X_TURN ? action.value == 'x' : action.value == 'o';
+    if(!correct_turn) return false;
+    bool in_bounds = action.column_index < size && action.row_index < size;
+    if(!in_bounds) return false;
+    bool is_free = board_state_(action.row_index, action.column_index) == '_';
+    if(!is_free) return false;
+    return true;
+  }
+
+  std::vector<TicTacToeAction> GetAvailableActions() const {
+    std::vector<TicTacToeAction> actions;
+    char turn_value = game_status_ == GameStatus::X_TURN ? 'x' : 'o';
+    for(int row_index = 0; row_index < size; row_index++) {
+      for(int column_index = 0; column_index < size; column_index++) {
+        if(board_state_(row_index, column_index) == '_') {
+          actions.push_back({row_index, column_index, turn_value});
+        }
+      }
+    }
+    return actions;
+  }
+
+  void ApplyAction(TicTacToeAction const& action) {
+    if(IsActionValid(action)) {
+      board_state_(action.row_index, action.column_index) = action.value;
+      x_turn_ = !x_turn_;
+      game_status_ = UpdateGameStatus(); 
     }
   }
 
-  //check columns
-  for(int column_index = 0; column_index < size; column_index++) {
-    if(CheckColumnValue(column_index, 'o')) {
-      return BoardStatus::O_WINS;
-    } else if(CheckColumnValue(column_index, 'x')) {
-      return BoardStatus::X_WINS;
-    }
+  GameStatus GetGameStatus() const {
+    return game_status_;
   }
 
-  //check diagonals
-  if(CheckFirstDiagonal('x') || CheckSecondDiagonal('x')) {
-    return BoardStatus::X_WINS;
-  } else if(CheckFirstDiagonal('o') || CheckSecondDiagonal('o')) {
-    return BoardStatus::O_WINS;
+  bool GameOver() const {
+    return game_status_ == GameStatus::X_WINS || game_status_ == GameStatus::O_WINS;
   }
+
+  private: 
+
+  GameStatus UpdateGameStatus() {
+    for(int row_index = 0; row_index < size; row_index++) {
+      if(CheckRowValue(row_index, 'o')) {
+        return GameStatus::O_WINS;
+      } else if(CheckRowValue(row_index, 'x')) {
+        return GameStatus::X_WINS;
+      }
+    }
+
+    for(int column_index = 0; column_index < size; column_index++) {
+      if(CheckColumnValue(column_index, 'o')) {
+        return GameStatus::O_WINS;
+      } else if(CheckColumnValue(column_index, 'x')) {
+        return GameStatus::X_WINS;
+      }
+    }
+
+    if(CheckFirstDiagonal('x') || CheckSecondDiagonal('x')) {
+      return GameStatus::X_WINS;
+    } else if(CheckFirstDiagonal('o') || CheckSecondDiagonal('o')) {
+      return GameStatus::O_WINS;
+    }
   
-  return BoardStatus::IN_PROGRESS;
+    return x_turn_ ? GameStatus::X_TURN : GameStatus::O_TURN;
   }
-  
-  bool CheckFirstDiagonal(char value) {
+
+  bool CheckFirstDiagonal(char value) const {
     for(int index = 0; index < size; index++) {
       if(board_state_(index, index) != value) {
         return false;
@@ -78,7 +129,7 @@ class TicTacToeBoard {
     return true;
   }
 
-  bool CheckSecondDiagonal(char value) {
+  bool CheckSecondDiagonal(char value) const {
     for(int index = 0; index < size; index++) {
       if(board_state_(size - index - 1, index) != value) {
         return false;
@@ -87,7 +138,7 @@ class TicTacToeBoard {
     return true;
   }
 
-  bool CheckRowValue(size_t row_index, char value) {
+  bool CheckRowValue(size_t row_index, char value) const {
     for(int column_index = 0; column_index < size; column_index++) {
       if(board_state_(row_index, column_index) != value) {
         return false;
@@ -96,7 +147,7 @@ class TicTacToeBoard {
     return true;
   }
 
-  bool CheckColumnValue(size_t column_index, char value) {
+  bool CheckColumnValue(size_t column_index, char value) const {
     for(int row_index = 0; row_index < size; row_index++) {
       if(board_state_(row_index, column_index) != value) {
         return false;
@@ -104,15 +155,19 @@ class TicTacToeBoard {
     } 
     return true;
   }
-
-  private:
+  
   BoardState board_state_;
+  GameStatus game_status_;
+  bool x_turn_;
 };
-
-
 
 int main(int argc, char* argv[])  {
   TicTacToeBoard<3> game;
+
+  game.ApplyAction({0, 0, 'x'});  
+  game.ApplyAction({1, 1, 'x'});  
+  game.ApplyAction({2, 2, 'x'});  
+
   std::cout << game.GetBoardState() << std::endl;
-  std::cout << to_string(game.CheckBoardStatus()) << std::endl;
+  std::cout << to_string(game.GetGameStatus()) << std::endl;
 }
