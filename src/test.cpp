b513c608820ec6d68b8635db5f4efbd098c4ad4c
psiorx <psiorx@gmail.com>
2017-08-01 20:27:45 -0400

    TemporalDifferenceAgent winning 95% of the time

--- src/test.cpp
+++ src/test.cpp
@@ -5,26 +5,37 @@
 #include <chrono>
 
 #include <Eigen/Dense>
-//a session consists of a game board and two agents
-//each agent takes a turn at modifying the board state
-//until a terminal state is reached
 
-enum class GameStatus {
+template<typename Iter, typename RandomGenerator>
+Iter select_randomly(Iter start, Iter end, RandomGenerator& g) {
+    std::uniform_int_distribution<> dis(0, std::distance(start, end) - 1);
+    std::advance(start, dis(g));
+    return start;
+}
+
+template<typename Iter>
+Iter select_randomly(Iter start, Iter end) {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    return select_randomly(start, end, gen);
+}
+
+enum class TicTacToeStatus {
     X_WINS,
     O_WINS,
     DRAW,
     IN_PROGRESS
 };
 
-std::string to_string(GameStatus status) {
+std::string to_string(TicTacToeStatus status) {
     switch(status) {
-    case GameStatus::X_WINS:
+    case TicTacToeStatus::X_WINS:
         return "X_WINS";
-    case GameStatus::O_WINS:
+    case TicTacToeStatus::O_WINS:
         return "O_WINS";
-    case GameStatus::DRAW:
+    case TicTacToeStatus::DRAW:
         return "DRAW";
-    case GameStatus::IN_PROGRESS:
+    case TicTacToeStatus::IN_PROGRESS:
         return "IN_PROGRESS";
     }
     return "UNKNOWN";
@@ -44,8 +55,11 @@ std::string to_string(TicTacToeAction const& action) {
 class TicTacToe {
 public:
     using Action = TicTacToeAction;
-    using Status = GameStatus;
+    using Status = TicTacToeStatus;
     using BoardStateType = Eigen::Matrix<char, 3, 3>;
+    
+    //TODO: implement TicTacToe(string state) ... initialize x_turn based on num_x-num_o
+
     TicTacToe() {
         Reset();
     };
@@ -55,7 +69,7 @@ public:
     }
 
     void Reset() {
-        game_status_ = GameStatus::IN_PROGRESS;
+        game_status_ = TicTacToeStatus::IN_PROGRESS;
         board_state_.fill('-');
         x_turn = true;
     }
@@ -75,7 +89,7 @@ public:
     void ApplyAction(TicTacToeAction const& action) {
         board_state_(action.row_index, action.column_index) = x_turn ? 'x' : 'o';
         x_turn = !x_turn;
-        game_status_ = UpdateGameStatus();
+        game_status_ = UpdateTicTacToeStatus();
     }
 
     TicTacToe ForwardModel(TicTacToeAction const& action) const {
@@ -84,12 +98,16 @@ public:
         return new_board;
     }
 
-    GameStatus GetGameStatus() const {
+    TicTacToeStatus GetGameStatus() const {
         return game_status_;
     }
 
     bool GameOver() const {
-        return game_status_ != GameStatus::IN_PROGRESS;
+        return game_status_ != TicTacToeStatus::IN_PROGRESS;
+    }
+
+    bool Draw() const {
+      return game_status_ == TicTacToeStatus::DRAW;
     }
 
     std::string GetStateString() const {
@@ -98,34 +116,34 @@ public:
 
 private:
 
-    GameStatus UpdateGameStatus() {
+    TicTacToeStatus UpdateTicTacToeStatus() {
         for(int row_index = 0; row_index < size; row_index++) {
             if(CheckRowValue(row_index, 'o')) {
-                return GameStatus::O_WINS;
+                return TicTacToeStatus::O_WINS;
             } else if(CheckRowValue(row_index, 'x')) {
-                return GameStatus::X_WINS;
+                return TicTacToeStatus::X_WINS;
             }
         }
 
         for(int column_index = 0; column_index < size; column_index++) {
             if(CheckColumnValue(column_index, 'o')) {
-                return GameStatus::O_WINS;
+                return TicTacToeStatus::O_WINS;
             } else if(CheckColumnValue(column_index, 'x')) {
-                return GameStatus::X_WINS;
+                return TicTacToeStatus::X_WINS;
             }
         }
 
         if(CheckFirstDiagonal('x') || CheckSecondDiagonal('x')) {
-            return GameStatus::X_WINS;
+            return TicTacToeStatus::X_WINS;
         } else if(CheckFirstDiagonal('o') || CheckSecondDiagonal('o')) {
-            return GameStatus::O_WINS;
+            return TicTacToeStatus::O_WINS;
         }
 
         if(BoardFull()) {
-            return GameStatus::DRAW;
+            return TicTacToeStatus::DRAW;
         }
 
-        return GameStatus::IN_PROGRESS;
+        return TicTacToeStatus::IN_PROGRESS;
     }
 
     bool CheckFirstDiagonal(char value) const {
@@ -176,9 +194,9 @@ private:
     }
 
     BoardStateType board_state_;
-    GameStatus game_status_;
+    TicTacToeStatus game_status_;
     const int size = 3;
-    const int string_size = size * size + 1;
+    const int string_size = size * size;
     bool x_turn;
 };
 
@@ -188,9 +206,15 @@ public:
     typename Game::Status PlayOnce() {
         while(true) {
             game.ApplyAction(player1.GetAction(game));
-            if(game.GameOver()) break;
+            if(game.GameOver()) {
+              player2.GetAction(game);
+              break;
+            }
             game.ApplyAction(player2.GetAction(game));
-            if(game.GameOver()) break;
+            if(game.GameOver()) {
+              player1.GetAction(game);
+              break;
+            }
         }
         return game.GetGameStatus();
     }
@@ -207,11 +231,20 @@ public:
 
     void Reset() {
         game.Reset();
+        player1.Reset();
+        player2.Reset();
+    }
+
+    Agent1<Game> GetPlayer1() const {
+      return player1;
+    }
+
+    Agent2<Game> GetPlayer2() const {
+      return player2;
     }
 
 private:
     Game game;
-
     Agent1<Game> player1;
     Agent2<Game> player2;
 };
@@ -224,52 +257,116 @@ public:
             return action;
         }
     }
+    void Reset() { }
 };
 
 template <class Game>
 class PickRandomActionAgent {
 
-    template<typename Iter, typename RandomGenerator>
-    Iter select_randomly(Iter start, Iter end, RandomGenerator& g) const {
-        std::uniform_int_distribution<> dis(0, std::distance(start, end) - 1);
-        std::advance(start, dis(g));
-        return start;
-    }
-
-    template<typename Iter>
-    Iter select_randomly(Iter start, Iter end) const {
-        static std::random_device rd;
-        static std::mt19937 gen(rd());
-        return select_randomly(start, end, gen);
-    }
-
 public:
     typename Game::Action GetAction(const Game& state) const {
         auto actions = state.GetAvailableActions();
+        if(actions.size() > 0)
         return *select_randomly(actions.begin(), actions.end());
     }
+
+    void Reset() { }
+};
+
+template <class Game>
+class TemporalDifferenceAgent {
+public:
+
+typename Game::Action GetBestAction(const Game& state) {
+  float best_value = 0;
+  typename Game::Action best_action;
+  std::string state_string;
+  for(auto const& action : state.GetAvailableActions()) {
+    Game next_state = state.ForwardModel(action);
+    float state_value = GetValue(next_state);
+    if(state_value > best_value) {
+      state_string = next_state.GetStateString();
+      best_value = state_value;
+      best_action = action;
+    }
+  }
+  float Vs = value_function[state_after_last_move];
+  float Vs_prime = best_value;
+  value_function[state_after_last_move] = Vs + alpha * (Vs_prime - Vs);
+  state_after_last_move = state_string; 
+  return best_action;
+}
+
+typename Game::Action GetAction(const Game& state) {
+  if(state_after_last_move.empty()) {
+    state_after_last_move = state.GetStateString();
+  }
+
+  if(state.GameOver() && !state.Draw()) {
+     std::string state_string = state.GetStateString();
+     float Vs = value_function[state_after_last_move];
+     value_function[state_after_last_move] = Vs - alpha * Vs;
+  }
+  auto actions = state.GetAvailableActions();
+
+  typename Game::Action action;
+  float random = static_cast <float> (rand()) / static_cast <float> (RAND_MAX); 
+  if(random < epsilon) {
+    action = *select_randomly(actions.begin(), actions.end());
+  } else {
+    action = GetBestAction(state);
+  }
+  //handle exploratory actions
+  return action;
+}
+
+void Reset() {
+  state_after_last_move = "";
+}
+
+private:
+  float GetValue(const Game& state) {
+    if(state.GameOver() && !state.Draw()) {
+      return 1.0f;
+    } 
+    std::string state_string = state.GetStateString();
+    if(value_function.find(state_string) == value_function.end()) {
+      value_function[state_string] = 0.5;
+    }
+    return value_function[state_string];
+  }
+
+const float alpha = 0.05;
+const float epsilon = 0.05;
+std::string state_after_last_move;
+std::unordered_map<std::string, float> value_function;
+
 };
 
 int main(int argc, char* argv[])  {
     using namespace std::chrono;
     int x_wins=0, o_wins=0, draws=0;
-    int num_games = 1e6;
+    int num_games = 100000;
+  
+    GameSession<TicTacToe, TemporalDifferenceAgent, PickRandomActionAgent> session;
 
-    GameSession<TicTacToe, PickRandomActionAgent, PickRandomActionAgent> session;
+    session.PlayN(100000);
     high_resolution_clock::time_point t1 = high_resolution_clock::now();
-    for(GameStatus status : session.PlayN(num_games)) {
-        switch(status) {
-        case GameStatus::X_WINS:
+    for(int i = 0; i < num_games ; i++) {
+        switch(session.PlayOnce()) {
+        case TicTacToeStatus::X_WINS:
             x_wins++;
             break;
-        case GameStatus::O_WINS:
+        case TicTacToeStatus::O_WINS:
             o_wins++;
             break;
-        case GameStatus::DRAW:
+        case TicTacToeStatus::DRAW:
             draws++;
             break;
         }
+        session.Reset();
     }
+
     high_resolution_clock::time_point t2 = high_resolution_clock::now();
     duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
     std::cout << "Played " << num_games/time_span.count() << " games per second." << std::endl;
