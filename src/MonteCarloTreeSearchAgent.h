80a1030e267e67a0bef28b51b91a12d2881865b0
psiorx <psiorx@gmail.com>
2017-08-14 09:50:24 -0400

    lots of prints, fixed bug in post-simulation score calculatoin

--- src/MonteCarloTreeSearchAgent.h
+++ src/MonteCarloTreeSearchAgent.h
@@ -5,6 +5,7 @@
 #include <memory>
 #include "utils.h"
 #include "Stopwatch.h"
+
 struct GameStats {
   int wins;
   int plays;
@@ -12,23 +13,39 @@ struct GameStats {
 
 template <class Game>
 struct TreeNode {
-  TreeNode(Game const& game, bool our_turn, std::shared_ptr<TreeNode<Game>> parent)
-    : stats({0, 0}), state(game), our_turn(our_turn), parent(parent) { 
+  TreeNode(Game const& game, 
+           bool our_turn, 
+           std::shared_ptr<TreeNode<Game>> parent,
+           typename Game::Action action)
+    : stats({0, 0}), state(game), our_turn(our_turn), parent(parent), action(action) { 
         unexplored_actions = state.GetAvailableActions();
         std::random_shuffle(unexplored_actions.begin(), unexplored_actions.end());
+
+        std::cout << "New search node created for state " << state.GetStateString() <<  std::endl; 
+        if(parent) {
+          std::cout << " with parent " << parent->state.GetStateString() << std::endl;
+        }
+  }
+
+  double WinRatio() const {
+    return stats.wins / (double) stats.plays;
   }
 
   double UpperConfidenceBound() const {
-    double win_percentage = stats.wins / (double) stats.plays;
+  double win_percentage = WinRatio();
+    
     if(!parent) {
       return win_percentage;
     }
-    return win_percentage + sqrt(2*log(parent->stats.plays) / stats.plays); 
+    double confidence_bound = sqrt(2*log(parent->stats.plays) / stats.plays);
+    //std::cout << "UCB: " << state.GetStateString() << " " << win_percentage << " +/- " << confidence_bound << std::endl;
+    return win_percentage + confidence_bound; 
   }
 
   GameStats stats;
   Game state;
   bool our_turn;
+  typename Game::Action action;
   std::vector<std::shared_ptr<TreeNode<Game>>> children;
   std::vector<typename Game::Action> unexplored_actions;
   std::shared_ptr<TreeNode<Game>> parent;
@@ -40,21 +57,46 @@ public:
   using TreeNodePtr = std::shared_ptr<TreeNode<Game>>;
   
   typename Game::Action GetAction(const Game& state) {
-    search_tree = std::make_shared<TreeNode<Game>>(state, true, nullptr);
+   search_tree = std::make_shared<TreeNode<Game>>(state, true, nullptr, typename Game::Action());
+   //SearchForTime(100.0);
+   SearchForIterations(20);
+
+   int best_value = 100;
+   typename Game::Action best_action;
+   for(auto const& child : search_tree->children) {
+     double value = child->WinRatio();
+     std::cout << child->state.GetStateString() << " : " << value << std::endl; 
+     if(value <= best_value) {
+       best_value = value;
+       best_action = child->action;
+     }
+   }
+
+   return best_action;
+  }
+
+  void SearchForTime(double ms) {
+   const int batch_size = 1000;
    Stopwatch sw;
    sw.Start();
    int iterations = 0;
-   while(sw.ElapsedMillis() < 1000) {
-     for(int x = 0; x < 10000; x++) {
-       MonteCarloTreeSearch(search_tree);
-       iterations++;
-     } 
+   while(sw.ElapsedMillis() < ms) {
+    SearchForIterations(batch_size);
+    iterations += batch_size;
    }
    sw.Stop();
-   std::cout << "Completed: "<< iterations << " iterations in " << sw.ElapsedMillis() << " ms" << std::endl;
+   //std::cout << "Completed: "<< iterations << " iterations in " << sw.ElapsedMillis() << " ms" << std::endl;
+  }
+
+  void SearchForIterations(int n) {
+    for(int i = 0; i < n; i++) {
+       std::cout << "########### " << i << " ###########" << std::endl;
+       MonteCarloTreeSearch(search_tree);
+     }   
   }
 
   TreeNodePtr Selection(TreeNodePtr node) {
+    std::cout << "Running Selection for state " << node->state.GetStateString() << std::endl;
     if(node == nullptr) {
       return node;
     }
@@ -65,6 +107,7 @@ public:
       return node;
     }
 
+    std::cout << "Bandit phase" << std::endl;
     //treat as bandit problem
     double best_value = -std::numeric_limits<double>::infinity();
     TreeNodePtr best_child = nullptr;
@@ -77,19 +120,23 @@ public:
     }
     
     if(!best_child) {
-      int score = GetScore(node);
-      Backpropagation(node, score);
-      return nullptr;
+     int score = GetScore(node);
+     std::cout << "Calling backprop from Selection" << std::endl;
+     Backpropagation(node, score);
+     return nullptr;
     }
      
     return Selection(best_child); 
   }
 
   TreeNodePtr Expansion(TreeNodePtr node) {
+
     auto action = node->unexplored_actions.back();
     node->unexplored_actions.pop_back();
+    std::cout << "Expanding action " << (int)action << " from state " << node->state.GetStateString() << std::endl;
+    
     Game next_state = node->state.ForwardModel(action);
-    auto child_node = std::make_shared<TreeNode<Game>>(next_state, !node->our_turn, node);
+    auto child_node = std::make_shared<TreeNode<Game>>(next_state, !node->our_turn, node, action);
     node->children.push_back(child_node);
     return child_node;
   }
@@ -97,13 +144,40 @@ public:
   int Simulation(TreeNodePtr node) {
     Game simulated_game = node->state;
     bool our_turn = node->our_turn;
+    
+    std::cout << "Running Simulation for state " 
+    << node->state.GetStateString() 
+    << (our_turn ? " (our turn)" : " (their turn)")   
+    << std::endl;
+
     while(!simulated_game.GameOver()) {
       auto actions = simulated_game.GetAvailableActions();
       simulated_game.ApplyAction(*select_randomly(actions.begin(), actions.end()));
       our_turn = !our_turn;
+      std::cout << "Simulation advanced to state " 
+      << simulated_game.GetBoardState() 
+      << (our_turn ? " (our turn)" : " (their turn)")   
+      << std::endl;      
+    }
+     
+    std::cout << "Simulation terminated in state " 
+    << simulated_game.GetBoardState() 
+    << (our_turn ? " (our turn)" : " (their turn)")   
+    << std::endl;      
+
+    int score;
+    
+    if(simulated_game.Draw()) {
+      std::cout << "Simulation ended in a draw (0)" << std::endl;
+      score = 0;
+    } else if(our_turn) {
+      std::cout << "Simulation ended in a loss (-1)" << std::endl;
+      score = -1;
+    } else {
+      std::cout << "Simulation ended in a win (1)" << std::endl;
+      score = 1;
     }
 
-    int score = GetScore(node);
     return score;
   }
 
@@ -116,13 +190,13 @@ public:
     } else {
       score = 1;
     }
+    std::cout << "Score for state: " << node->state.GetBoardState() << " on " << (node->our_turn ? "our_turn" : "their turn") << " = " <<  score << std::endl;
+
     return score;
   }
 
   void Backpropagation(TreeNodePtr node, int score) {
-    if(!node) {
-      return;
-    }
+    std::cout << "Running Backprop for state " << node->state.GetStateString() << " with score: " << score << std::endl;
 
     node->stats.plays++;
     if(node->our_turn && score > 0) {
@@ -130,8 +204,12 @@ public:
     } else if(!node->our_turn && score < 0) {
       node->stats.wins++;
     }
-    //std::cout << "New score for state :" << node->state.GetStateString() << ": " << node->stats.wins << "/" << node->stats.plays << " (" << node->UpperConfidenceBound() << ")" << std::endl;
-    Backpropagation(node->parent, score);
+
+    std::cout << "New score for state " << node->state.GetStateString() << ": " << node->stats.wins << "/" << node->stats.plays << " (" << node->UpperConfidenceBound() << ")" << std::endl;
+    if(node->parent) {
+      std::cout << "Backprop recursing from " << node->state.GetStateString() << " -> " << node->parent->state.GetStateString() << std::endl;
+      Backpropagation(node->parent, score);
+    }
   }
 
   GameStats MonteCarloTreeSearch(TreeNodePtr node) {
@@ -140,8 +218,7 @@ public:
       TreeNodePtr expanded_node = Expansion(unexpanded_child);
       double reward = Simulation(expanded_node);
       Backpropagation(expanded_node, reward);
-    } //maybe call backprop here directly in the nullptr case
-    
+    } 
   }
 
   void Reset() { }
