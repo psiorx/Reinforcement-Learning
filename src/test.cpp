93c00f59e04f812dcee96e20b4a993ad23da39e9
psiorx <psiorx@gmail.com>
2017-08-14 09:50:47 -0400

    monte carlo agent wins the test game

--- src/test.cpp
+++ src/test.cpp
@@ -6,51 +6,62 @@
 #include "MinimaxAgent.h"
 #include "TemporalDifferenceAgent.h"
 #include "MonteCarloTreeSearchAgent.h"
+#include "TestGame.h"
 #include "Stopwatch.h"
 
 int main(int argc, char* argv[])  {
-    std::srand ( unsigned ( std::time(0) ) );
+    //std::srand ( unsigned ( std::time(0) ) );
+    std::srand (123);
+    
     int x_wins=0, o_wins=0, draws=0;
-    int num_games = 10000;
+    int num_games = 1;
 
-    GameSession<TicTacToe, TemporalDifferenceAgent, MinimaxAgent> session;
+    // TestGame game;
+    // MonteCarloTreeSearchAgent<TestGame> test;
+    // test.GetAction(game);
+    // exit(0);
 
-    TicTacToe test;
-    MonteCarloTreeSearchAgent<TicTacToe> agent;
-    agent.GetAction(test);
-    exit(0);
-    Stopwatch sw;
-
-    sw.Start();
-    for(int i = 0; i < num_games ; i++) {
-        switch(session.PlayOnce()) {
-        case TicTacToeStatus::X_WINS:
-            x_wins++;
-            break;
-        case TicTacToeStatus::O_WINS:
-            o_wins++;
-            break;
-        case TicTacToeStatus::DRAW:
-            draws++;
-            break;
-        }
-        session.Reset();
+    GameSession<TestGame, MonteCarloTreeSearchAgent, PickRandomActionAgent> session;
+    auto status = session.PlayOnce();
+    if(status == TestGameStatus::WIN) {
+        std::cout << "We won" << std::endl;
+    } else {
+        std::cout << "We lost" << std::endl;
     }
-    sw.Stop();
+    // GameSession<TicTacToe, MonteCarloTreeSearchAgent, PickRandomActionAgent> session;
 
-    std::cout << "Played " 
-    << num_games/sw.ElapsedMillis()*1000 
-    << " games per second." << std::endl;
+    // Stopwatch sw;
 
-    std::cout << "x_wins: " 
-    << x_wins/(float)num_games*100 
-    << std::endl;
+    // sw.Start();
+    // for(int i = 0; i < num_games ; i++) {
+    //     switch(session.PlayOnce()) {
+    //     case TicTacToeStatus::X_WINS:
+    //         x_wins++;
+    //         break;
+    //     case TicTacToeStatus::O_WINS:
+    //         o_wins++;
+    //         break;
+    //     case TicTacToeStatus::DRAW:
+    //         draws++;
+    //         break;
+    //     }
+    //     session.Reset();
+    // }
+    // sw.Stop();
 
-    std::cout << "o_wins: " 
-    << o_wins/(float)num_games*100 
-    << std::endl;
+    // std::cout << "Played " 
+    // << num_games/sw.ElapsedMillis()*1000 
+    // << " games per second." << std::endl;
 
-    std::cout << "draws: " 
-    << draws/(float)num_games*100 
-    << std::endl;
+    // std::cout << "x_wins: " 
+    // << x_wins/(float)num_games*100 
+    // << std::endl;
+
+    // std::cout << "o_wins: " 
+    // << o_wins/(float)num_games*100 
+    // << std::endl;
+
+    // std::cout << "draws: " 
+    // << draws/(float)num_games*100 
+    // << std::endl;
 }
